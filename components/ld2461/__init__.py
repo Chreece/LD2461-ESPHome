7aeb31259d46c39c66ba6431122ba246b0b5d93d
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2024-01-14 12:30:41 +0100

    dev

--- components/ld2461/__init__.py
+++ components/ld2461/__init__.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID
 
-from .const import CONF_LD2461_ID, CONF_INVERT_X, CONF_INVERT_Y
+from .const import CONF_LD2461_ID
 
 DEPENDENCIES = ["uart"]
 MULTI_CONF = True
@@ -14,17 +14,11 @@ LD2461 = ld2461_ns.class_("LD2461", cg.PollingComponent, uart.UARTDevice)
 PresenceRegion = ld2461_ns.class_("PresenceRegion")
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(LD2461),
-    cv.Optional(CONF_INVERT_X, default=False): cv.boolean,
-    cv.Optional(CONF_INVERT_Y, default=False): cv.boolean,
+    cv.GenerateID(): cv.declare_id(LD2461)
 }).extend(cv.polling_component_schema("1s")).extend(uart.UART_DEVICE_SCHEMA)
 
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    if invert_x := config.get(CONF_INVERT_X):
-        cg.add_define("INVERT_X")
-    if invert_y := config.get(CONF_INVERT_Y):
-        cg.add_define("INVERT_Y")
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
