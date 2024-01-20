807ac3309330de0afdec3ddab3bea51c2ab04a65
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2024-01-20 14:47:40 +0100

    dev

--- components/ld2461/number/__init__.py
+++ components/ld2461/number/__init__.py
@@ -2,9 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import number
 from esphome.const import (
-    DEVICE_CLASS_EMPTY,
-    UNIT_DEGREES,
-    ICON_ROTATE_RIGHT,
     ENTITY_CATEGORY_CONFIG,
     CONF_INITIAL_VALUE,
     CONF_RESTORE_VALUE,
@@ -22,7 +19,7 @@ from esphome.const import (
 )
 from .. import LD2461, ld2461_ns, PresenceRegion
 
-from ..const import (CONF_ROTATE, CONF_PRESENCE_TIMEOUT, CONF_PRESENCE_REGIONS,
+from ..const import (CONF_PRESENCE_TIMEOUT, CONF_PRESENCE_REGIONS,
     CONF_ENTRY_POINTS, CONF_X0, CONF_Y0, CONF_X1, CONF_Y1, CONF_X, CONF_Y,
     CONF_LD2461_ID)
 
@@ -30,12 +27,6 @@ NUMBERS = [CONF_X0, CONF_Y0, CONF_X1, CONF_Y1]
 
 DEPENDENCIES = ["ld2461"]
 
-RotateNumber = ld2461_ns.class_(
-    "RotateNumber",
-    number.Number,
-    cg.Component,
-    cg.Parented.template(LD2461)
-)
 PresenceTimeoutNumber = ld2461_ns.class_(
     "PresenceTimeoutNumber",
     number.Number,
@@ -51,20 +42,6 @@ EntryPoint = ld2461_ns.class_("EntryPoint")
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_LD2461_ID): cv.use_id(LD2461),
-    cv.Optional(CONF_ROTATE): number.NUMBER_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(RotateNumber),
-            cv.Optional(
-                CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_CONFIG
-            ): cv.entity_category,
-            cv.Optional(CONF_INITIAL_VALUE, default=0): cv.positive_float,
-            cv.Optional(CONF_MAX_VALUE, default=360): cv.positive_float,
-            cv.Optional(CONF_MIN_VALUE, default=0): cv.positive_float,
-            cv.Optional(CONF_RESTORE_VALUE, default=True): cv.boolean,
-            cv.Optional(CONF_STEP, default=0.1): cv.positive_float,
-            cv.Optional(CONF_UNIT_OF_MEASUREMENT, default=UNIT_DEGREES): cv.string_strict
-        }
-    ).extend(cv.COMPONENT_SCHEMA),
     cv.Optional(CONF_PRESENCE_TIMEOUT): number.NUMBER_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(PresenceTimeoutNumber),
@@ -146,22 +123,7 @@ CONFIG_SCHEMA = CONFIG_SCHEMA.extend(
 
 async def to_code(config):
     ld2461 = await cg.get_variable(config[CONF_LD2461_ID])
-    if rotate_config := config.get(CONF_ROTATE):
-        var = await number.new_number(
-            rotate_config,
-            min_value=rotate_config[CONF_MIN_VALUE],
-            max_value=rotate_config[CONF_MAX_VALUE],
-            step=rotate_config[CONF_STEP],
-        )
-        await cg.register_component(var, rotate_config)
-        cg.add(var.set_initial_value(rotate_config[CONF_INITIAL_VALUE]))
-        cg.add(var.set_restore_value(rotate_config[CONF_RESTORE_VALUE]))
 
-        hub = await cg.get_variable(config[CONF_LD2461_ID])
-        func = getattr(hub, f"set_rotate_number")
-        cg.add(func(var))
-        cg.add(var.set_parent(hub))
-    
     if presence_timeout_config := config.get(CONF_PRESENCE_TIMEOUT):
         var = await number.new_number(
             presence_timeout_config,
