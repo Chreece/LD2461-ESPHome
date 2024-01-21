f9326a4797422f0820a4f0cf36b27d8f863d3c24
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2024-01-21 12:00:11 +0100

    dev

--- components/ld2461/ld2461.h
+++ components/ld2461/ld2461.h
@@ -147,6 +147,7 @@ class LD2461 : public uart::UARTDevice, public PollingComponent {
     void send_command_(uint8_t command_str, const uint8_t *command_value, int command_value_len);
     void get_version_();
     void get_regions_();
+    void get_reporting_();
 };
 
 
